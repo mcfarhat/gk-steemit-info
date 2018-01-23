6faeda49bd2c65497a9095c50a222c2ff2097738
Mohammad Farhat <mcfarhat@gmail.com>
2018-01-23 15:22:01 +0200

    Add files via upload
    additional functionality includes:
    support for multiple instances of same widget on screen as well as with shortcode on same screen.
    filtering of steemit posts by tag, to allow display of only relevant posts on a page/widget
    filtering of posts by minimum amount of post payout
    new option to exclude resteems from the user's posts display
    display of number of upvotes for each posts
    support for font awesome on the plugin

--- gk-steemit-info.php
+++ gk-steemit-info.php
@@ -3,7 +3,7 @@
   Plugin Name: GK Steemit Info
   Plugin URI: http://www.greateck.com/
   Description: A wordpress plugin that allows adding steemit (www.steemit.com) data to wordpress sites via widget or alternatively a shortcode
-  Version: 0.1.0
+  Version: 0.2.0
   Author: mcfarhat
   Author URI: http://www.greateck.com
   License: GPLv2
@@ -95,6 +95,8 @@ function steemit_count_renderer($refresh_frequency){
 
 	if (!$libraries_appended){
 ?>
+		<!-- including fontawesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<!-- including steemjs library for performing calls -->
 		<script src="https://cdn.steemjs.com/lib/latest/steem.min.js"></script>
 		<!-- including jQuery -->
@@ -154,11 +156,21 @@ class steemit_user_posts_widget extends WP_Widget {
 			echo $args['before_title'] . $title . $args['after_title'];
 			
 		//grab username and post count params
-		$username = apply_filters( 'steemit_username_widget_title', $instance['steemit_username'] );
-		$title = apply_filters( 'steemit_post_count_widget_title', $instance['steemit_post_count'] );
+		$username = apply_filters( 'steemit_username_widget', $instance['steemit_username'] );
+		$postcount = apply_filters( 'steemit_post_count_widget', $instance['steemit_post_count'] );
+		$excluderesteem = $instance['steemit_exclude_resteem'] ? true : false ;
+		$postminpay = apply_filters( 'steemit_post_min_pay_widget', $instance['steemit_post_min_pay'] );
+		$posttag = apply_filters( 'steemit_post_tag_widget', $instance['steemit_post_tag'] );
 		
+		//set as false by default
+		if ($excluderesteem==''){
+			$excluderesteem = false;
+		}
+		//widget container unique identifier based on timestamp
+		$date = new DateTime();
+		$contentid = $date->getTimestamp().mt_rand(1,4000);
 		//display output in the widget
-		steemit_user_posts_renderer($username, $postcount);
+		steemit_user_posts_renderer($username, $postcount, $excluderesteem, $postminpay, $posttag, $contentid);
 		
 		//making room for hook display by any theme
 		echo $args['after_widget'];
@@ -170,7 +182,7 @@ class steemit_user_posts_widget extends WP_Widget {
 			$title = $instance[ 'title' ];
 		}
 		else {
-			$title = __( 'Latest Posts on <a href="https://www.steemit.com">Steemit</a>', 'gk_steemit_user_posts' );
+			$title = __( 'Posts on <a href="https://www.steemit.com">Steemit</a>', 'gk_steemit_user_posts' );
 		}
 		if ( isset( $instance[ 'steemit_username' ] ) ) {
 			$steemit_username = $instance[ 'steemit_username' ];
@@ -178,17 +190,33 @@ class steemit_user_posts_widget extends WP_Widget {
 		if ( isset( $instance[ 'steemit_post_count' ] ) ) {
 			$steemit_post_count = $instance[ 'steemit_post_count' ];
 		}
+		if ( isset( $instance[ 'steemit_exclude_resteem' ] ) ) {
+			$steemit_exclude_resteem = $instance[ 'steemit_exclude_resteem' ];
+		}
+		if ( isset( $instance[ 'steemit_post_min_pay' ] ) ) {
+			$steemit_post_min_pay = $instance[ 'steemit_post_min_pay' ];
+		}
+		if ( isset( $instance[ 'steemit_post_tag' ] ) ) {
+			$steemit_post_tag = $instance[ 'steemit_post_tag' ];
+		}
+		
 		
 		// Widget admin form
 		?>
 		<p>
 		<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+		<input type="text" class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
 		</p>
 		<p><label for="<?php echo $this->get_field_id( 'steemit_username' ); ?>">Steemit Username:</label>
-		@<input class="text" id="<?php echo $this->get_field_id( 'steemit_username' ); ?>" name="<?php echo $this->get_field_name( 'steemit_username' ); ?>" type="text" value="<?php echo esc_attr( $steemit_username ); ?>"></p>
-		<p><label for="<?php echo $this->get_field_id( 'steemit_post_count' ); ?>">Max Posts:</label>
-		<input class="text" id="<?php echo $this->get_field_id( 'steemit_post_count' ); ?>" name="<?php echo $this->get_field_name( 'steemit_post_count' ); ?>" type="number" step="1" min="1" max="50" value="<?php echo esc_attr( $steemit_post_count ); ?>"></p>		
+		@<input type="text" class="text" id="<?php echo $this->get_field_id( 'steemit_username' ); ?>" name="<?php echo $this->get_field_name( 'steemit_username' ); ?>" value="<?php echo esc_attr( $steemit_username ); ?>"></p>
+		<p><label for="<?php echo $this->get_field_id( 'steemit_post_count' ); ?>">Max Post Count:</label>
+		<input type="number" class="text" id="<?php echo $this->get_field_id( 'steemit_post_count' ); ?>" name="<?php echo $this->get_field_name( 'steemit_post_count' ); ?>" step="1" min="1" max="50" value="<?php echo esc_attr( $steemit_post_count ); ?>"></p>
+		<p><label for="<?php echo $this->get_field_id( 'steemit_post_tag' ); ?>">Filter by Tag:</label>
+		<input type="text" class="text" id="<?php echo $this->get_field_id( 'steemit_post_tag' ); ?>" name="<?php echo $this->get_field_name( 'steemit_post_tag' ); ?>" value="<?php echo esc_attr( $steemit_post_tag ); ?>"></p>
+		<p><label for="<?php echo $this->get_field_id( 'steemit_exclude_resteem' ); ?>">Exclude Resteems:</label>
+		<input type="checkbox" id="<?php echo $this->get_field_id( 'steemit_exclude_resteem' ); ?>" name="<?php echo $this->get_field_name( 'steemit_exclude_resteem' ); ?>" <?php if ($steemit_exclude_resteem){ echo "checked";}?>></p>
+		<p><label for="<?php echo $this->get_field_id( 'steemit_post_min_pay' ); ?>">Minimum Pay:</label>
+		<input type="number" class="text" id="<?php echo $this->get_field_id( 'steemit_post_min_pay' ); ?>" name="<?php echo $this->get_field_name( 'steemit_post_min_pay' ); ?>" step="1" min="1" max="200" value="<?php echo esc_attr( $steemit_post_min_pay ); ?>"></p>
 		<?php
 	}
 	// Updating widget replacing old instances with new
@@ -197,6 +225,9 @@ class steemit_user_posts_widget extends WP_Widget {
 		$instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
 		$instance['steemit_username'] = ( ! empty( $new_instance['steemit_username'] ) ) ? $new_instance['steemit_username'] : '';
 		$instance['steemit_post_count'] = ( ! empty( $new_instance['steemit_post_count'] ) ) ? $new_instance['steemit_post_count'] : '';
+		$instance['steemit_exclude_resteem'] = $new_instance['steemit_exclude_resteem'];
+		$instance['steemit_post_min_pay'] = ( ! empty( $new_instance['steemit_post_min_pay'] ) ) ? $new_instance['steemit_post_min_pay'] : '0';
+		$instance['steemit_post_tag'] = ( ! empty( $new_instance['steemit_post_tag'] ) ) ? $new_instance['steemit_post_tag'] : '';
 		return $instance;
 	}
 }
@@ -208,7 +239,7 @@ function gk_load_steemit_user_posts_widget() {
 add_action( 'widgets_init', 'gk_load_steemit_user_posts_widget' ); 
  
 /* shortcode to display steemit user count on front end. 
-Use it in format [steemit_user_posts username=USERNAME limit=LIMIT] */
+Use it in format [steemit_user_posts username=USERNAME limit=LIMIT excluderesteem=1 minpay=0 filtertag=TAG] */
 add_shortcode('steemit_user_posts', 'display_steemit_user_posts' );
 
 function display_steemit_user_posts( $atts, $content = "" ) {
@@ -217,11 +248,27 @@ function display_steemit_user_posts( $atts, $content = "" ) {
     ), $atts );*/
 	$username = $inner_atts['username'];
 	$postcount = $inner_atts['limit'];
-	steemit_user_posts_renderer($username, $postcount);
+	$excluderesteem = $inner_atts['excluderesteem'];
+	$postminpay = $inner_atts['postminpay'];
+	$posttag = $inner_atts['filtertag'];
+	//default to true
+	if (empty($excluderesteem)){
+		$excluderesteem = '';
+	}
+	if (empty($posttag)){
+		$posttag = '';
+	}
+	if (empty($postminpay) || $postminpay==''){
+		$postminpay = 0;
+	}
+	//widget container unique identifier based on timestamp
+	$date = new DateTime();
+	$contentid = $date->getTimestamp().mt_rand(1,4000);;
+	steemit_user_posts_renderer($username, $postcount, $excluderesteem, $postminpay, $posttag, $contentid);
 }
 
 /* function handling the display of the selected users' posts */
-function steemit_user_posts_renderer($username, $postcount){
+function steemit_user_posts_renderer($username, $postcount, $excluderesteem, $postminpay, $posttag, $contentid){
 	if ($username == ''){
 		echo 'Steemit username not provided';
 		return;
@@ -232,6 +279,8 @@ function steemit_user_posts_renderer($username, $postcount){
 	}
 	if (!$libraries_appended){
 ?>
+		<!-- including fontawesome -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 		<!-- including steemjs library for performing calls -->
 		<script src="https://cdn.steemjs.com/lib/latest/steem.min.js"></script>
 		<!-- including jQuery -->
@@ -240,8 +289,19 @@ function steemit_user_posts_renderer($username, $postcount){
 		$libraries_appended = true;
 	}
 ?>
+
+	<style>
+		.gk-loader-img{
+			display: block;
+			margin: auto;
+		}
+	</style>
 	<div>
-		<div id="user_posts_container"></div>
+		<div id="user_posts_container<?php echo $contentid;?>">
+			<!-- default loader -->
+			<img class="gk-loader-img" src="<?php echo plugins_url();?>/gk-steemit-info/img/ajax-loader.gif">
+		</div>
+		
 	</div>	
 	<script>
 			/* when properly loaded, call steem API method to grab recent posts by selected username and display them */
@@ -251,22 +311,66 @@ function steemit_user_posts_renderer($username, $postcount){
 				
 				//setup query to grab proper user with preset limit
 				var query = {
+					/* tag here actually accepts the username as param */
 					tag: '<?php echo $username;?>',
-					limit: <?php echo $postcount;?>,
+					/* setting higher hard-coded limit to allow for further filtering on additional details, since the API seems to fail without a set limit for now. 
+					User set limit will be handled manually in the loop..
+					For speeing things up, we will test if any filtering criteria is not set, and the limit is less than 100, then use that limit instead for API call */
+					<?php
+						$hard_limit = 100;
+						if (($postminpay=='' || $postminpay == 0 )&& ($excluderesteem == '' || $excluderesteem ==false) && ($posttag == '')){
+							$hard_limit = $postcount;
+						}
+					?>
+					limit: <?php echo $hard_limit;?>,
 				};
-				var container = document.getElementById('user_posts_container');
+				var container = document.getElementById('user_posts_container<?php echo $contentid;?>');
 				//call getDiscussionsByBlog to grab user's latest posts
 				steem.api.getDiscussionsByBlog(query, function (err, posts) {
 					// console.log(err, discussions);
 					if (!err) {
-						posts.map(function (post) {
-							console.log(post);
-							//create a new entry
-							var entry = document.createElement('div');
-							entry.setAttribute('class','steemit-post-entry');
-							//grab the details of the post
-							var post_details = '<a href="https://www.steemit.com'+post.url+'">'+post.title+'</a>';
-							//append money value onto it
+						var included_post_count = 0;
+						var post_limit = <?php echo $postcount;?>;
+						var exclude_resteem = '<?php echo $excluderesteem; ?>';
+						var current_author = '<?php echo $username;?>';
+						var min_pay = '<?php echo $postminpay; ?>';
+						var filter_tag = '<?php echo $posttag; ?>';
+						
+						//remove loader / empty container
+						container.innerHTML="";
+						
+						/* replacing map with each to allow breaking out */
+						$.each (posts, function (index, post){
+						//posts.map(function (post) {
+							// console.log(post);
+							var post_json_meta = JSON.parse(post.json_metadata);
+							// console.log(post_json_meta.tags);
+							
+							//images: post_json_meta.image
+							/* tests for inclusion */
+							
+							/* test 1: if checkbox to exclude resteems is not set, skip resteems */
+							if (exclude_resteem != ''){
+								if (post.author != current_author){
+									// console.log('exclude_resteem:'+exclude_resteem);
+									// console.log('post.author:'+post.author+'>current_author:'+current_author);
+									//skip
+									return true;
+								}
+							}
+							
+							/* test 2: checking if post has correct tag to be included */
+							//tags: post_json_meta.tags
+							if (filter_tag != ''){
+								//if the tag is not to be found within the posts' tags, skip it
+								if ($.inArray(filter_tag, post_json_meta.tags)<0){
+									// console.log(filter_tag);
+									// console.log(post_json_meta.tags);								
+									//skip
+									return true;
+								}
+							}
+							
 							//grab payout value as default, if the post has been paid
 							var money_val = post.total_payout_value;
 							//check if author rewards have been paid or not, if not we need to grab the pending amount alternatively
@@ -277,10 +381,45 @@ function steemit_user_posts_renderer($username, $postcount){
 							}
 							money_val = money_val.replace('SBD','$');
 							money_val = money_val.replace('STEEM','$');
+							
+							/* test 3: checking if a min amount is set for inclusion, if so and post is less, skip */
+							if (min_pay!=''){
+								if (parseInt(min_pay)>0){
+									tested_amount = money_val.replace('$','');
+									if (parseInt(tested_amount)<parseInt(min_pay)){
+										// console.log('found a lesser value, skip');
+										// console.log('post amount:'+tested_amount+'>min amount:'+min_pay);
+										//skip
+										return true;
+									}
+								}
+							}
+							
+							//create a new entry
+							var entry = document.createElement('div');
+							entry.setAttribute('class','steemit-post-entry');
+							
+							//grab the details of the post
+							var post_details = '<a href="https://www.steemit.com'+post.url+'">'+post.title;
+							
+							//append vote count onto it
+							// console.log(post.active_votes.length);
+							post_details += '&nbsp; ('+post.active_votes.length+' <i class="fa fa-thumbs-up" aria-hidden="true"></i>)';
+							
+							//append money value onto it
 							post_details += '&nbsp; ('+money_val+')';
+							
+							//close href
+							post_details += '</a>';
 							entry.innerHTML = post_details;
 							//append it to the existing list
 							container.appendChild(entry);
+							//increase count of included posts
+							included_post_count ++;
+							if (included_post_count>=post_limit){
+								//break out
+								return false;
+							}
 						});
 					}
 				});
